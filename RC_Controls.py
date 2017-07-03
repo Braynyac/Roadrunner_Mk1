1cc47833be8eb7607d8c7caf136b4b01539552ed
Bryan Garcia <bryan.garcia.b9r@gmail.com>
2017-07-03 20:02:18 +0000

    Add files via upload
    *Throttle control

--- RC_Controls.py
+++ RC_Controls.py
@@ -2,17 +2,17 @@ import RPi.GPIO as GPIO
 import time
 
 
-servo_signal = 3 # BCM GPIO number
-throttle_signal = 2 # BCM GPIO number
+servo_signal = 3
+throttle_signal = 2
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(servo_signal, GPIO.OUT)
 GPIO.setup(throttle_signal, GPIO.OUT)
 servo_pwm = GPIO.PWM(servo_signal, 100)
-throttle_pwm = GPIO.PWM(throttle_signal, 100)
 servo_pwm.start(0)
+throttle_pwm = GPIO.PWM(throttle_signal, 100)
 throttle_pwm.start(0)
 
-# A clone function of the arduino's map()
+
 def translate(value, leftMin, leftMax, rightMin, rightMax):
     # Figure out how 'wide' each range is
     leftSpan = leftMax - leftMin
@@ -22,13 +22,33 @@ def translate(value, leftMin, leftMax, rightMin, rightMax):
     # Convert the 0-1 range into a value in the right range.
     return rightMin + (valueScaled * rightSpan)
 
-# updates angle and speed in a single function, default speed is zero for safety precaution
+
+def limit(num,minn,maxx):
+    if num<minn:
+        num=minn
+    elif num>maxx:
+        num=maxx
+    return num
+
+def throttle_control(speed):
+    speed = limit(speed,-100,100)
+    if speed == 0:
+        return 13.5
+    elif speed<0:
+        return translate(speed,-100,0,4.25,12)
+    elif speed>0:
+        return translate(speed,0,100,14,24)
+
 def update(angle,speed=0):
-    # 10.7 and 20 is what I experimetally found to be the correct modulations between 0 and 180 degrees of the servo
     duty = translate(float(angle), 0,180,20,10.7)
     servo_pwm.ChangeDutyCycle(duty)
     throttle_pwm.ChangeDutyCycle(float(speed))
 
 
 def shutdown():
-    GPIO.cleanup() # cleans up the pins, but is not currently implemented
+    GPIO.cleanup()
+
+if __name__ == '__main':    
+    while True:
+        i = float(input("Speed:"))
+        update(90,throttle_control(i))
